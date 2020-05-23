9e5d32a30b72a7439c405eae0fa9ed70f460ee38
Barenziah <lunasecrete@gmail.com>
2020-05-23 17:07:12 +0200

    Fix asyncronicity in Gandalf uwu

--- commands/gandalf.js
+++ commands/gandalf.js
@@ -7,11 +7,12 @@ module.exports = {
   execute(msg, args) {
     const voiceChannel = msg.member.voiceChannel
     if (typeof voiceChannel !== "undefined") {
-      voiceChannel.join().then(connection => {
-        const dispatcher = connection.playFile(path.join(soundsDir, 'gandalf.wav'), { volume: 0.5 })
-        msg.channel.send('Do not take me for some conjurer of cheap tricks !', { files: ["https://i.imgur.com/azbSS3q.gif"] })
-        dispatcher.on("end", end => voiceChannel.leave());
-      }).catch(err => console.error(err));
+      msg.channel.send('Do not take me for some conjurer of cheap tricks !', { files: ["https://i.imgur.com/azbSS3q.gif"] }).then(() => {
+        voiceChannel.join().then(connection => {
+          const dispatcher = connection.playFile(path.join(soundsDir, 'gandalf.wav'), { volume: 0.5 })
+          dispatcher.on("end", end => voiceChannel.leave());
+        }).catch(err => console.error(err));
+      })
     }
   }
 };
