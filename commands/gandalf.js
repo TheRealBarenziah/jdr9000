879ed7e342e7f59693de80a475804d9e87dcdb65
Oriane Sasseur <presciente.snotrq@gmail.com>
2020-05-25 13:50:22 +0200

    Update documentation

--- commands/gandalf.js
+++ commands/gandalf.js
@@ -3,7 +3,7 @@ const soundsDir = require("../assets/sounds/soundsDir");
 
 module.exports = {
   name: "gandalf",
-  description: "Post a Gandalf gif. If you are in a voice chat, he will also come to say a line.",
+  description: "If you are in a voice chat, post a gif, and Gandalf will come to say a line in your voicecom.",
   execute(msg) {
     const voiceChannel = msg.member.voiceChannel;
     if (typeof voiceChannel !== "undefined") {
--- commands/judgement.js
+++ commands/judgement.js
@@ -3,7 +3,7 @@ const soundsDir = require("../assets/sounds/soundsDir");
 
 module.exports = {
   name: "judgement",
-  description: "Will play Judgement Knights of Thunder in your voicecom !",
+  description: "If you're in voicecom, this command will come and play Judgement Knights of Thunder ! (use `/stop` to stop it)",
   execute(msg) {
     const voiceChannel = msg.member.voiceChannel;
     if (typeof voiceChannel !== "undefined") {
