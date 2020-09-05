241ad67ae72b5c8dc5b571b6f9577ab4615902c7
Barenziah <lunasecrete@gmail.com>
2020-09-05 02:24:01 +0200

    Fix Gandalf not playing voice

--- commands/gandalf.js
+++ commands/gandalf.js
@@ -9,16 +9,14 @@ module.exports = {
     if (typeof voiceChannel !== "undefined") {
       msg.channel.send("Do not take me for some conjurer of cheap tricks !", { files: ["https://i.imgur.com/azbSS3q.gif"] })
         .then(() => {
-          if(voiceChannel.members.map(m => m.bot).length <= 1){
-            voiceChannel.join()
-              .then(connection => {
-                const dispatcher = connection.playFile(path.join(soundsDir, "gandalf.wav"), { volume: 0.5 });
-                dispatcher.on("end", () => {
-                  voiceChannel.leave();
-                });
-              })
-              .catch(e => console.error(e));
-          }
+          voiceChannel.join()
+            .then(connection => {
+              const dispatcher = connection.playFile(path.join(soundsDir, "gandalf.wav"), { volume: 0.5 });
+              dispatcher.on("end", () => {
+                voiceChannel.leave();
+              });
+            })
+            .catch(e => console.error(e));
         }).catch(e => console.error(e));
     }
     else msg.channel.send("Thou shalt be in a voice channel to use this, foool !");
