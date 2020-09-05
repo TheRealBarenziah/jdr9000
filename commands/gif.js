ca097172a7e4726c04cc9cf8861340a43d6cada5
Oriane Sasseur <presciente.snotrq@gmail.com>
2020-09-06 01:17:53 +0200

    Add cutting-edge functionality onto master

--- commands/gif.js
+++ commands/gif.js
@@ -1,20 +1,32 @@
 const prefix = require("../utils/prefix");
+const soundsDir = require("../assets/sounds/soundsDir");
 
 module.exports = {
   name: "gif",
   description: `Display some gifs .\nAvailable commands:\n\`${prefix}gif ah`,
+  
   execute(msg, args) {
-    /*
-    const invokerId = msg.author.id;
-    const invokerUsername = msg.author.username;
-    const invokerHashtag = msg.author.discriminator;
-    */
     if (args.length === 0) {
       msg.author.send(`${this.description} `);
     }
     else if (args[0] === "ah") {
-      msg.channel.send(file = "https://media.giphy.com/media/3o7btW7VDxqrhJEnqE/giphy.gif")
-        .catch(e => console.error(e));
+        const voiceChannel = msg.member.voiceChannel;
+        if (Boolean(voiceChannel)) {
+          msg.channel.send(file = "https://media.giphy.com/media/3o7btW7VDxqrhJEnqE/giphy.gif")
+          .then(() => {
+              voiceChannel.join()
+                .then(connection => {
+                  const dispatcher = connection.playFile(path.join(soundsDir, "gandalf.wav"), { volume: 0.6 });
+                  dispatcher.on("end", () => {
+                    voiceChannel.leave();
+                  });
+                })
+                .catch(e => console.error(e));
+            }).catch(e => console.error(e));
+        }
+        else {
+          msg.channel.send(file = "https://media.giphy.com/media/3o7btW7VDxqrhJEnqE/giphy.gif");
+      }
     }
   }
 };
