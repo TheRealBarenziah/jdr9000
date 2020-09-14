bf5cd3fd1c05bf4e9ed820877e71828a2991c5f2
therealbarenziah <presciente.snotrq@gmail.com>
2020-09-14 20:20:46 +0200

    feat: roll by default
    -Add "default" behavior for /roll command without param (retrocompatibility)
    -Sweeten output in case of several dices (total displayed at bottom)

--- commands/roll.js
+++ commands/roll.js
@@ -8,10 +8,14 @@ module.exports = {
   name: "roll",
   description: `Takes an argument \`${prefix}roll XdY\` where X is the number of dices you want, & Y the number of faces for the dices.`,
   execute(msg, args) {
+    if (args.length === 1 && ( Number(args[0]) <= 100 && Number(args[0]) >= 0)) {
+      msg.reply(` rolling 1d${args[0]} :\n` + colorizeText.orange(roll(args[0]).toString()));
+      return null;
+    }
     const parsedArg = args[0].split("d");
     if (parsedArg.length !== 2) {
       msg.channel.send(`Argument format invalid: must be \`${prefix}roll XdY\` where X is the number of dices & Y the number of faces for each dice !`);
-      return 0;
+      return null;
     }
     else {
       let diceQty = parsedArg[0];
@@ -20,21 +24,24 @@ module.exports = {
         if ( ( Number(diceQty) <= 100) && ( Number(diceSize) <= 1000) ){
           // lets be safe with user input Q_Q..
 
-          let results = [];
+          const results = [];
           for (let i = 0; i < diceQty; i++) {
             results.push(roll(diceSize));
           }
-          msg.channel.send("Rolling " + diceQty + "d" + diceSize + ". Results :\n" + colorizeText.orange(results.toString()));
+          const sum = results.reduce((a, b) => a + b, 0);
+
+          msg.reply(" rolling " + diceQty + "d" + diceSize + ":\n" + colorizeText.orange(results.toString() + `\nTotal: ${sum}`));
         }
         else {
           msg.channel.send(`Argument invalid: must be \`${prefix}roll XdY\` where X is maximum 100 & Y is maximum 1000 !`);
-          return 0;
+          return null;
         }
       }
       else {
         msg.channel.send(`Argument invalid: must be \`${prefix}roll XdY\` where X and Y are numeric values !`);
-        return 0;
+        return null;
       }
     }
+    return null;
   },
 };
