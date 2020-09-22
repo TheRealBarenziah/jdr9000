3544c7b35042ffa874740fa0e8dda6031c1b7c84
Barenziah <lunasecrete@gmail.com>
2020-09-22 20:08:45 +0200

    fix: previous fix wasnt enough

--- commands/gif.js
+++ commands/gif.js
@@ -12,6 +12,7 @@ module.exports = {
   execute(msg, args) {
     if (args.length === 0) {
       msg.author.send(`${this.description} `);
+      return null;
     }
     else if (args[0] === "ah") {
       const voiceChannel = msg.member.voiceChannel;
@@ -25,35 +26,46 @@ module.exports = {
             });
           })
           .catch(e => console.error(e));
+        return null;
       }
       else {
         msg.channel.send(file = "https://media.giphy.com/media/3o7btW7VDxqrhJEnqE/giphy.gif");
+        return null;
       }
     }
 
     else {
-
       const querystring = args.join().replace(/,/g, " ");
       // console.log("querystring : ", querystring);
-
-      giphy.search({
+      return giphy.search({
         q: querystring,
         rating: "r",
-        limit: 8/*,
-        fmt: "json"*/
+        limit: 8
+        /*
+        ,fmt: "json"
+        */
       }, function (err, res) {
         // console.log("res", res);
-        if (err) console.error(err);
+        if (err) return console.error(err);
         // console.log(res.data);
-        if(res && res.data){
-          if(res.data.length > 5){
+        if(res.data){
+          if(res.data.length > 1){
             const randomized = roll(res.data.length) - 1;
             msg.channel.send(file = res.data[randomized].url);
+            return null;
           }
-          else if (res.data[0].url) {
+          else if (res.data[0]) {
             msg.channel.send(file = res.data[0].url);
+            return null;
           }
-          else msg.reply("fokoff modafoka i didnt find ur shit!");
+          else if (typeof res.data[0] === "undefined") {
+            msg.reply("... no result were found :(");
+            return null;
+          }
+        }
+        else {
+          msg.reply("fokoff modafoka i didnt find ur shit!");
+          return null;
         }
       });
     }
