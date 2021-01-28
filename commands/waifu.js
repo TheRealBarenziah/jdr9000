661f7f0fe0f1ceb95ec6167329132b6d7a15cfe3
TheRealBarenziah <presciente.snotrq@gmail.com>
2021-01-28 16:19:43 +0100

    feat: significant improvement of "waifu" capability (owo)

--- commands/waifu.js
+++ commands/waifu.js
@@ -1,4 +1,5 @@
 const waifuGenerator = require("waifu-generator");
+const faker = require("faker");
 const tfaker = require("tfaker");
 const Discord = require("discord.js");
 
@@ -9,9 +10,10 @@ module.exports = {
     if (args.length === 0) {
       const base64_img = await waifuGenerator({ skipFs: true });
       const waifuName = tfaker.firstName();
+      const gibberish = faker.hacker.phrase();
       const sfbuff = new Buffer.from(await base64_img.split(",")[1], "base64");
-      const x = new Discord.Attachment(sfbuff, `${waifuName}.png`);
-      await msg.channel.send(`Ohaio! My name is ${waifuName}`, x)
+      const base64Attach = new Discord.Attachment(sfbuff, `${waifuName}.png`);
+      await msg.channel.send(`Ohaio! ${waifuName} at your service.\n${gibberish} ${Math.floor(Math.random() * 2) === 0 ? "owo" : "uwu"}`, base64Attach)
         .catch(e => console.error(e));
     }
     else msg.channel.send("I dont accept any kind of arguments...");
