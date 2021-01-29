a9992d12118adf5e38c22f3bde80c804ae083efb
TheRealBarenziah <presciente.snotrq@gmail.com>
2021-01-29 21:57:48 +0100

    fix: uncomment heapdump :)

--- commands/heapdump.js
+++ commands/heapdump.js
@@ -2,7 +2,6 @@ const heapdump = require("heapdump");
 const path = require("path");
 
 module.exports = {
-  /*
   name: "heapdump",
   description: "Only works if `NODE_ENV=DEVELOPMENT`. Pass `!` as argument to write a heapsnapshot into the ./heapdumps folder. Pass `gc` as argument to manually call the garbage collector (only works if environment variable `NODE_ENV` equals `development`)",
   execute(msg, args) {
@@ -15,10 +14,10 @@ module.exports = {
         console.log("Proc garbage collector");
         global.gc();
       }
-      else if ((args.length === 1) && (args[0] === "env")) {
-        msg.author.send(`My environment is currently set to '${process.env.NODE_ENV}'`);
-      }
+      else return;
+    }
+    else if ((args.length === 1) && (args[0] === "env")) {
+      msg.author.send(`My environment is currently set to '${process.env.NODE_ENV}'`);
     }
   }
-  */
 };
