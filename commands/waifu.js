3f12d2f6ccddb5de1a0a81be5ee95a05585033a0
TheRealBarenziah <presciente.snotrq@gmail.com>
2021-02-15 13:15:25 +0100

    feat: waifu now accepts "--uwu" argument

--- commands/waifu.js
+++ commands/waifu.js
@@ -11,18 +11,26 @@ const sentience = () => {
     :
     faker.name.firstName();
 
-    const gibberish = () => {
-    switch (roll(4)) {
-      case 1:
-        return uwufy(faker.hacker.phrase()).replace(">w<", "")
-      case 2:
-        return uwufy(faker.company.bs()).replace(">w<", "");
-      case 3:
-        return faker.hacker.phrase();
-      default:
-        return faker.company.bs();
+  const gibberish = (uwufication) => {
+
+    const chaos = roll(2);
+    if (uwufication === true) {
+      switch (chaos) {
+        case 1:
+          return uwufy(faker.hacker.phrase()).replace(">w<", "");
+        default:
+          return uwufy(faker.company.bs()).replace(">w<", "");
+      }
     }
-  }
+    else {
+      switch (chaos) {
+        case 1:
+          return faker.hacker.phrase();
+        default:
+          return faker.company.bs();
+      }
+    }
+  };
 
   const uwu = () => {
     switch (roll(4)) {
@@ -35,7 +43,7 @@ const sentience = () => {
       default:
         return "";
     }
-  }
+  };
 
   const greetings = () => {
     switch (roll(4)) {
@@ -48,25 +56,33 @@ const sentience = () => {
       default:
         return `Hello? I'm ${waifuName()}.`;
     }
-  }
+  };
 
   return {
     gibberish: gibberish,
     uwu: uwu,
     greetings: greetings
-  }
-}
+  };
+};
 
 module.exports = {
   name: "waifu",
-  description: "Display a random anime girl.",
+  description: "Display a random anime girl. Accept a single '--uwu' argument. >w<",
   async execute(msg, args) {
+    const s = sentience();
+    const base64_img = await waifuGenerator({ skipFs: true });
+    const sfbuff = new Buffer.from(await base64_img.split(",")[1], "base64");
+    const base64Attach = new Discord.Attachment(sfbuff, "ohaio.png");
     if (args.length === 0) {
-      const s = sentience();
-      const base64_img = await waifuGenerator({ skipFs: true });
-      const sfbuff = new Buffer.from(await base64_img.split(",")[1], "base64");
-      const base64Attach = new Discord.Attachment(sfbuff, `ohaio.png`);
-      await msg.channel.send(`${s.greetings()}\n${s.gibberish()} ${s.uwu()}`, base64Attach)
+      await msg.channel.send(`${s.greetings()}\n${s.gibberish(false)} ${s.uwu()}`, base64Attach)
+        .catch(e => console.error(e));
+    }
+    else if (args.length === 1 && (
+      args[0] === "--uwu"
+      ||
+      args[0] === "-u"
+    )) {
+      await msg.channel.send(`${s.greetings()}\n${s.gibberish(true)} ${s.uwu()}`, base64Attach)
         .catch(e => console.error(e));
     }
     else msg.channel.send("I dont accept any kind of arguments...");
