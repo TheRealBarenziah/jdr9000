10dd2712b716ac97ee04cdb741cc363eef2e77ca
TheRealBarenziah <presciente.snotrq@gmail.com>
2021-07-29 13:15:13 +0200

    feat: improve man UX (now accepts a "--public" | "-p" | "-public" flag

--- utils/man.js
+++ utils/man.js
@@ -8,7 +8,14 @@ module.exports = {
   description: "Manual for jdr9000. Can take a command as argument.",
   execute(msg, args) {
     let commands = ["man command"];
-    const arg = args[0];
+    const cmd = args[0];
+
+    const pushMsg = (args, string) => {
+      if (args.includes("--public") || args.includes("-p") || args.includes("-public")){
+        msg.channel.send(string);
+      }
+      else msg.author.send(string);
+    }; 
 
     fs.readdirSync(commandsDir)
       .forEach((file) => {
@@ -18,40 +25,41 @@ module.exports = {
           null;
       });
 
-    if ((args[0] === "man") && (args.length === 1)) {
+    if ((cmd === "man") && (args.length === 1)) {
       msg.author.send(this.description);
     }
 
-    else if ((args[0] === "jdr") && (args.length === 2)) {
+    else if ((args[0] === "jdr") && (args.length >= 2)) {
+
       if (args[1] === "init") {
-        msg.author.send(`Init takes one argument: your roleplay name. Ex: \`${prefix}jdr init Clem\``);
+        pushMsg(args, `Init takes one argument: your roleplay name. Ex: \`${prefix}jdr init Clem\``);
       }
       else if (args[1] === "updateStat") {
-        msg.author.send(`That command takes 2 another arguments: the stat you want to modify (format: mainStat.subStat) and the new value. Ex: \`${prefix}jdr updateStat force.puissance 80\` will set your puissance to 80\n__Stats (mainStat is first)__:\n**adresse**(artisanat, visee, minutie)\n**agilite**(souplesse, reflexe, mouvement)\n**force**(athletisme, puissance, constitution)\n**social**(aura, parole, sangFroid)\n**magie**(puissance, adresse, reflexe)\n**savoirMagique**(perception, education, intuition)\n**artsDeLaGuerre**(perception, education, pratique)`);
+        pushMsg(args, `That command takes 2 another arguments: the stat you want to modify (format: mainStat.subStat) and the new value. Ex: \`${prefix}jdr updateStat force.puissance 80\` will set your puissance to 80\n__Stats (mainStat is first)__:\n**adresse**(artisanat, visee, minutie)\n**agilite**(souplesse, reflexe, mouvement)\n**force**(athletisme, puissance, constitution)\n**social**(aura, parole, sangFroid)\n**magie**(puissance, adresse, reflexe)\n**savoirMagique**(perception, education, intuition)\n**artsDeLaGuerre**(perception, education, pratique)`);
       }
       else if (args[1] === "stats") {
-        msg.author.send("It displays your stats... But since you dared, here's a nyan cat just for you:", { files: ["https://media.giphy.com/media/sIIhZliB2McAo/giphy.gif"] });
+        pushMsg(args, "It displays your stats... But since you dared, here's a nyan cat just for you:", { files: ["https://media.giphy.com/media/sIIhZliB2McAo/giphy.gif"] });
       }
       else if (args[1] === "createSkill") {
-        msg.author.send("Don't try me");
+        pushMsg(args, "Don't try me");
       }
       else if (args[1] === "updateSkill") {
-        msg.author.send("Outrageous! This incident will be reported...");
+        pushMsg(args, "Outrageous! This incident will be reported...");
       }
       else if (args[1] === "deleteSkill") {
-        msg.author.send("Get lost :o");
+        pushMsg(args, "Get lost :o");
       }
       else if (args[1] === "levelup") {
-        msg.author.send(`Displays a GIF depending on argument.\nAvailable arguments: Eihnavé, Brasseur, Clem, Herumor, JB.\nExample: \`${prefix}jdr levelup Eihnavé\``);
+        pushMsg(args, `Displays a GIF depending on argument.\nAvailable arguments: Eihnavé, Brasseur, Clem, Herumor, JB.\nExample: \`${prefix}jdr levelup Eihnavé\``);
       }
     }
 
-    else if ((arg === "jdr9000") || (args.length === 0)) {
-      msg.author.send(`\`\`\`Available commands:\n${commands.map(c => `${prefix}${c}\n`).join("")}\n\`\`\``);
+    else if ((cmd === "jdr9000")) {
+      pushMsg(args, `\`\`\`Available commands:\n${commands.map(c => `${prefix}${c}\n`).join("")}\n\`\`\``);
     }
 
-    else if (commands.includes(arg)) {
-      msg.author.send(`${arg}: ${jdr9000[arg].description}`);
+    else if (commands.includes(cmd)) {
+      pushMsg(args, `${cmd}: ${jdr9000[cmd].description}`);
     }
     else {
       msg.author.send(`Command invalid: try **${prefix}man jdr9000** (or **${prefix}man *command*** to learn about a specific command)`);
