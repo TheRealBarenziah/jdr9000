308eb04c92622167e9e8848b858aae60715c6102
TheRealBarenziah <presciente.snotrq@gmail.com>
2021-10-08 21:37:42 +0200

    feat: better 'man' for eval
    & implement POWERUSERS check for heapdump

--- commands/eval.js
+++ commands/eval.js
@@ -2,7 +2,7 @@ const evaluate = require("../utils/eval");
 
 module.exports = {
   name: "eval",
-  description: "The nasty 'global.eval()' function in all its glory. Pass code as argument and watch the world burn.",
+  description: "The nasty 'global.eval()' function in all its glory. Pass code as argument and watch the world burn.\nAccepts a single '-p' (public mode) flag BEFORE the code.",
   execute(msg, args) {
     evaluate(msg, args);
     return null;
--- commands/heapdump.js
+++ commands/heapdump.js
@@ -1,23 +1,23 @@
 const heapdump = require("heapdump");
 const path = require("path");
+const isAuthorPoweruser = require("../utils/isAuthorPoweruser");
 
 module.exports = {
   name: "heapdump",
-  description: "Only works if `NODE_ENV=DEVELOPMENT`. Pass `!` as argument to write a heapsnapshot into the ./heapdumps folder. Pass `gc` as argument to manually call the garbage collector (only works if environment variable `NODE_ENV` equals `development`)",
+  description: "Only available to POWERUSERS. Pass `!` as argument to write a heapsnapshot into the ./heapdumps folder. Pass `gc` as argument to manually call the garbage collector (see full config in README)",
   execute(msg, args) {
-    if (process.env.NODE_ENV === "development") {
-
+    if (isAuthorPoweruser(msg)) {
       if ((args.length === 1) && (args[0] === "!")) {
-        heapdump.writeSnapshot(path.join(__dirname, `../heapdumps/${process.env.HOSTNAME}.${Date.now()}.heapsnapshot`));
+        heapdump.writeSnapshot(path.join(__dirname, `../heapdumps/${process.env.HOSTNAME ? process.env.HOSTNAME : "Heapdump"}.${Date.now()}.heapsnapshot`));
       }
       else if ((args.length === 1) && (args[0] === "gc")) {
         console.log("Proc garbage collector");
         global.gc();
       }
-      else return;
     }
-    else if ((args.length === 1) && (args[0] === "env")) {
-      msg.author.send(`My environment is currently set to '${process.env.NODE_ENV}'`);
+    else {
+      msg.author.send("How dare you use this command? Contact your nearest printer/wifi expert for further assistance.");
     }
+    return null;
   }
 };
