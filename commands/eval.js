16ab59b06e0d47891c80bdf3656a799e89ce4350
TheRealBarenziah <presciente.snotrq@gmail.com>
2021-10-16 11:43:44 +0200

    fix: better man for eval & sh

--- commands/eval.js
+++ commands/eval.js
@@ -1,8 +1,15 @@
 const evaluate = require("../utils/eval");
+const prefix = require("../utils/prefix");
 
 module.exports = {
   name: "eval",
-  description: "The nasty 'global.eval()' function in all its glory. Pass code as argument and watch the world burn.\nAccepts a single '-p' (public mode) flag BEFORE the code.",
+  description: `The nasty 'global.eval()' function in all its glory. Pass code as argument and watch the world burn. It will output return value in chat.
+Accepts a single '-p' (public mode) flag BEFORE the code. Can parse as string (\`${prefix}eval 1 + 1\`) or 'js' codeblock as below:
+${prefix}eval
+\`\`\`js
+js = () => "1 + 1";
+eval(js());
+\`\`\``,
   execute(msg, args) {
     evaluate(msg, args);
     return null;
--- commands/sh.js
+++ commands/sh.js
@@ -1,8 +1,10 @@
 const exec = require("../utils/sh");
+const prefix = require("../utils/prefix");
 
 module.exports = {
   name: "sh",
-  description: "Exec shell commands using nodejs `child_process`",
+  description: `Exec shell commands using nodejs \`child_process.exec\`. Will answer output in chat: message the bot to keep it private :wink:
+Example: \`${prefix + "sh ls -la | grep rw"}\``,
   async execute(msg, args) {
     await exec(msg, args);
     return null;
